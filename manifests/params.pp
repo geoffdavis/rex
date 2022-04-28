d6c79b0e220dc3f924551dfb1cffb6d0eccf9424
Geoff Davis <gadavis@ucsd.edu>
2022-04-27 18:59:23 -0700

    Correct root home directory
    Default user is root, so the home directory should be /root on applicable
    platforms.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,19 @@
 # If REX user has already been configured, you can override rex_user with the following...
 # <%= @host.params['remote_execution_ssh_user'] %>
 class rex::params (
-  $rex_user = 'root',
-  $rex_pw = '!!',
-  $rex_grp = 'wheel',
-  $rex_home = "/home/${rex_user}",
-  $rex_pw_max = '99999',
-  $rex_pw_min = '0',
-  $rex_comment = 'REX user for Satellite/Foreman',
-  $rex_keys = [],
-  $rex_dir = '/var/tmp',
+  String $rex_user = 'root',
+  String $rex_pw = '!!',
+  String $rex_grp = 'wheel',
+  Stdlib::Absolutepath $rex_home = $rex_user == 'root' ? {
+    true => '/root',
+    default => "/home/${rex_user}",
+  },
+  Variant[Integer,String] $rex_pw_max = '99999',
+  Variant[Integer,String] $rex_pw_min = '0',
+  String $rex_comment = 'REX user for Satellite/Foreman',
+  Array[String] $rex_keys = [],
+  Stdlib::Absolutepath $rex_dir = '/var/tmp',
 ) {
+  include stdlib
 }
 
