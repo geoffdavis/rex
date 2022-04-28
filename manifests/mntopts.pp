b583db9a2b75731f3553ed603173ad00de76287d
Geoff Davis <gadavis@ucsd.edu>
2022-04-27 18:48:54 -0700

    Make mntopts work on Ubuntu
    /bin and /usr/bin are different on some versions of Ubuntu.

--- manifests/mntopts.pp
+++ manifests/mntopts.pp
@@ -1,7 +1,12 @@
+# Validate the mount options for the remote execution user's homedirectory.
+#
+# Checks to make sure that the remote user's home directory is not mounted with noexec.
 class rex::mntopts (
   $rex_dir = $rex::params::rex_dir,
-  ) inherits rex::params {
-    exec { "/usr/bin/findmnt -n -o OPTIONS --target ${rex_dir} | grep noexec":
-      returns => 1,
-    }
+) inherits rex::params {
+  exec { "Check noexec on ${rex_dir}":
+    path    => ['/usr/sbin', '/sbin', '/usr/bin', '/bin'],
+    command => "findmnt -n -o OPTIONS --target ${rex_dir} | grep noexec",
+    returns => 1,
+  }
 }
